dd397c9b4530af0b4ba6c483ef7acec49b13c818
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-09-22 20:04:34 +0200

    Changed class name

--- src/test/java/gun1/DriverKurulumu.java
+++ src/test/java/gun1/DriverKurulumu.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Trendyol {
+public class DriverKurulumu {
     public static void main(String[] args) {
 
         WebDriver chromeDriver = new ChromeDriver();
