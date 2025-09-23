98f38f18d76ba5d5f5ccadb0c471a5522536139e
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-09-23 21:23:02 +0200

    Junit added

--- src/test/java/gun2/XpathOrnekleri.java
+++ src/test/java/gun2/XpathOrnekleri.java
@@ -1,5 +1,6 @@
 package gun2;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,5 +14,12 @@ public class XpathOrnekleri {
         WebElement productBtn =
                 driver.findElement(By.xpath("//ul[@class='nav navbar-nav']/li[2]/a"));
         productBtn.click();
+
+        WebElement allProductsText = driver.findElement(By.xpath("//div[@class='features_items']/h2"));
+
+        String expectedText = "ALL PRODUCTS";
+        String actualText = allProductsText.getText();
+
+        Assert.assertEquals("Products sayfasi acilmadi", expectedText, actualText);
     }
 }
