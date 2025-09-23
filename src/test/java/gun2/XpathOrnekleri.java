78408e073716fce8d0d0b6b09b4b7c538917edb8
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-09-23 20:26:33 +0200

    BaseDriver created

---
+++ src/test/java/gun2/XpathOrnekleri.java
@@ -0,0 +1,9 @@
+package gun2;
+
+import utility.BaseDriver;
+
+public class XpathOrnekleri {
+    public static void main(String[] args) {
+        BaseDriver.driver("https://automationexercise.com/");
+    }
+}
---
+++ src/test/java/utility/BaseDriver.java
@@ -0,0 +1,31 @@
+package utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseDriver {
+
+    public static void driver(String url) {
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+
+        try {
+            WebElement element =
+                    driver.findElement(By.xpath("//div[@class='fc-footer-buttons']/button[1]"));
+
+            element.click();
+            if (!element.isEnabled()) {
+                throw new NoSuchElementException("Element sayfada yok");
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Bilinmiyen bir hata olustu");
+        }
+
+    }
+}
